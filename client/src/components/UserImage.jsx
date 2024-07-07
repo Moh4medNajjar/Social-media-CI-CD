5aeb2a107944f27a031d18dc2f3d27163e0239a1
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-07-07 01:53:33 +0100

    fixed localhost in files

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`http://20.250.160.48:3001/assets/${image}`}
       />
     </Box>
   );
